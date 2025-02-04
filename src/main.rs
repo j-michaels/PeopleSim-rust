37183dd91d5ad4b573b8e63ebec6bfecf555c792
Joseph Michaels <joseph.michaels@bluenile.com>
2025-02-03 23:25:21 -0800

    Fixed move_person call

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, io::stdout, rc::Rc, vec};
+use std::{cell::RefCell, cmp::Ordering, io::stdout, rc::Rc, vec};
 use rand::prelude::*;
 
 const HOBBIES: [&'static str; 8] = [
@@ -22,13 +22,14 @@ struct Person {
     partying: bool,
 }
 
+#[derive(Copy, Clone)]
 struct Point {
     x: usize,
     y: usize,
 }
 
 struct World {
-    grid: Vec<Vec<Location>>,
+    grid: Vec<Vec<Rc<RefCell<Location>>>>,
     people: Vec<Rc<RefCell<Person>>>,
 }
 
@@ -47,7 +48,7 @@ fn make_world(width: usize, height: usize, n_people: u16) -> World {
         for _j in 0..height {
             let location = Vec::new();
 
-            row.push(location);
+            row.push(Rc::new(RefCell::new(location)));
         }
         world.grid.push(row);
     }
@@ -57,11 +58,11 @@ fn make_world(width: usize, height: usize, n_people: u16) -> World {
         
         person.hobbies.push(rand_hobby());
         person.coords = rand_location(&world);
-
+        let coords = person.coords;
         let rc = Rc::new(RefCell::new(person));
 
         world.people.push(Rc::clone(&rc));
-        get_at_world_coords(&mut world, &rc.borrow().coords).unwrap().push(Rc::clone(&rc));
+        world.grid[coords.x][coords.y].borrow_mut().push(Rc::clone(&rc));
     }
 
     return world;
@@ -89,8 +90,8 @@ fn is_compatible(person_a: &Person, person_b: &Person) -> bool {
 }
 
 fn in_bounds(world: &World, coords: &Point) -> bool {
-    return coords.x >= 0 && coords.x <= world.grid.len()
-        && coords.y >= 0 && coords.y <= world.grid.get(coords.x).unwrap().len()
+    return coords.x <= world.grid.len()
+        && coords.y <= world.grid.get(coords.x).unwrap().len()
 }
 
 fn find_adjacent_points(world: &World, coords: &Point, allow_diagonal: bool) -> Vec<Point> {
@@ -131,17 +132,17 @@ fn make_person(id: i32) -> Person {
     }
 }
 
-fn move_person(world: &mut World, person: &mut Person, destination: &Point) -> () {
-    let destination_group = get_at_world_coords(world, destination).unwrap();
-
-    let origin_group = get_at_world_coords(world, &person.coords).unwrap();
-
-    if let Some(index) = origin_group.iter().position(|p| p.borrow().id == person.id) {
-        if let Some(person_ref_cell) = origin_group.get(index).cloned() {
-            person.coords.x = destination.x;
-            person.coords.y = destination.y;
-            destination_group.push(person_ref_cell);
-            origin_group.remove(index);
+fn move_person(world: &mut World, person: &mut Person, dest: &Point) {
+    let origin = &person.coords;
+    let origin_rc = &world.grid[origin.x][origin.y];
+    let dest_rc = &world.grid[dest.x][dest.y];
+    
+    if let Some(index) = origin_rc.borrow().iter().position(|p| p.borrow().id == person.id) {
+        if let Some(person_ref_cell) = origin_rc.borrow().get(index).cloned() {
+            person.coords.x = dest.x;
+            person.coords.y = dest.y;
+            dest_rc.borrow_mut().push(person_ref_cell);
+            origin_rc.borrow_mut().remove(index);
         }
     }
 }
@@ -149,21 +150,9 @@ fn move_person(world: &mut World, person: &mut Person, destination: &Point) -> (
 /* Returns the same value as FindAdjacents() except without any empty locations. */
 fn find_adjacent_populated(world: &World, coords: &Point, allow_diagonal: bool) -> Vec<Point> {
     let mut adjacents = find_adjacent_points(world, coords, allow_diagonal);
-
-    adjacents.retain(|p| world.grid[p.x][p.y].len() > 0);
+    adjacents.retain(|p| world.grid[p.x][p.y].borrow().len() > 0);
 
     return adjacents;
-}
-
-fn get_at_world_coords<'a>(world: &'a mut World, coords: &Point) -> Result<&'a  mut Location, String> {
-    if coords.y <= world.grid.len() {
-        let row = &mut world.grid[coords.y];
-        if coords.x <= row.len() {
-            return Ok(&mut row[coords.x]);
-        }
-    }
-
-    return Err("Out of bounds".to_string());
 }
 
 fn iterate(world: &mut World, n_iterations: u8) {
@@ -171,13 +160,28 @@ fn iterate(world: &mut World, n_iterations: u8) {
         let adjacent_people = find_adjacent_populated(world, &person.borrow().coords, false);
         
         for adjacent_point in adjacent_people {
-            let loc = &world.grid[adjacent_point.x][adjacent_point.y];
-            for other_person in loc {
+            let loc = &world.grid[adjacent_point.x][adjacent_point.y].borrow();
+
+            for other_person in loc.iter() {
                 if is_compatible(&person.borrow(), &other_person.borrow()) {
-                    person_add_friend(&person.borrow_mut(), &other_person.borrow_mut())
+                    if let Some(other_person_rc) = world.people.iter().find(|&rc| Rc::ptr_eq(rc, person)) {
+                        person.borrow_mut().friends.push(other_person_rc.clone());
+                    }
                 }
             }
         }
+    }
+
+    let people_len = world.people.len();
+    for i in 0..people_len {
+        // let adjacents = find_adjacent_points(world, &world.people[i].borrow().coords, false);
+
+        // let destination = *adjacents.choose(&mut rand::thread_rng()).unwrap();
+        let destination = Point {
+            x: 0, y: 0
+        };
+        let person_rc = world.people[i].clone();
+        move_person(world, &mut person_rc.borrow_mut(), &destination);
     }
 }
 
@@ -190,8 +194,9 @@ fn main() {
 
     // say(&message, width, &mut writer).unwrap();
 
-    let world = make_world(20, 20, 10);
+    let mut world = make_world(20, 20, 10);
 
+    iterate(&mut world, 8);
 
 }
 
